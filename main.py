5ddeebaa9b54d810c6448e4c4c0cacfd0f512a1c
Bradley Delaune <bmdelaune@gmail.com>
2013-03-01 10:09:06 -0600

    I butchered your code but got the entropy out of it

--- main.py
+++ main.py
@@ -1,41 +1,42 @@
+import re
+import sys
+from math import log
+f = open(sys.argv[1],"r")
+text = f.read()
+f.close()
 print "Welcome to the Entropy Caclulator"
 
-file_to_work_with = raw_input("Please enter a filename to analyze: ")
+#file_to_work_with = raw_input("Please enter a filename to analyze: ")
 
-file = open(file_to_work_with,"r")
+#file = open(file_to_work_with,"r")
 
-text = file.read()
+#text = file.read()
 
-file.close()
+#file.close()
 
-print text+"\n"
-
+#print text+"\n"
+text = re.sub(r'([^\s\w]|_)+', '', text)
+text = text.lower()
+total_letters = len(text)
 a = {}
 
 for i, c in enumerate(text):
-	if c<>"\n":
 		if c in a:
 			a[c] = a[c] + 1
 		else:
 			a[c] = 1
-	if i+1 < len(text):
-		if text[i]<>"\n" and text[i+1]<>"\n":
-			if text[i]+text[i+1] in a:
-				a[text[i]+text[i+1]] = a[text[i]+text[i+1]] + 1
-			else:
-				a[text[i]+text[i+1]] = 1
-	if i+2 < len(text):
-		if text[i]<>"\n" and text[i+1]<>"\n" and text[i+2]<>"\n":
-			if text[i]+text[i+1]+text[i+2] in a:
-				a[text[i]+text[i+1]+text[i+2]] = a[text[i]+text[i+1]+text[i+2]] + 1
-			else:
-				a[text[i]+text[i+1]+text[i+2]] = 1
 			
-output = open(file_to_work_with+".entropy","w")
-
+output = open(sys.argv[1]+".entropy","w")
+sum = 0.0
 for x, y in sorted(a.iteritems()):
 	print x, y
 	output.write(str(x)+": "+str(y)+"\n")
-
+	pt = float(y) #converts key to float
+	pb = float(total_letters) #converts length to float
+	sum = sum + (pt/pb)*(log(pt/pb)) #calculates p(x) * log(p(x)) and adds to sum
 output.close()
+
+sum = sum*-1
+
+print sum
 	
--- refined.py
+++ refined.py
@@ -1,5 +1,6 @@
 import sys
 import re
+from math import log
 from collections import Counter
 
 f = open(sys.argv[1],"r")
@@ -8,10 +9,19 @@ f.close()
 
 text = re.sub(r'([^\s\w]|_)+', '', text) #keeps only alpha/nums/space
 refined = [x.lower() for x in text.split()] # makes list of lowercase words
+total_words = len(refined)
 count = Counter(refined) #counts the num of occurances for each word
 
 output = open(sys.argv[1] + ".entropy","w")
+sum = 0.0
 for key in count.most_common(len(count)): #saves them to file, sorted from most to least frequent
 	output.write(key[0] + ' : ' + str(key[1]) + '\n')
+	pt = float(key[1]) #converts key to float
+	pb = float(total_words) #converts length to float
+	sum = sum + (pt/pb)*(log(pt/pb)) #calculates p(x) * log(p(x)) and adds to sum
 output.close()
+
+sum = sum*-1
+
+print "The entropy of "+sys.argv[1]+" is "+str(sum)+"."
 	
