78c6ef32d8fc71d199865423e2d27407101f6226
Bradley Delaune <bmdelaune@gmail.com>
2013-03-19 18:27:19 -0500

    all books have license info removed

--- main.py
+++ main.py
@@ -6,15 +6,6 @@ text = f.read()
 f.close()
 print "Welcome to the Entropy Caclulator"
 
-#file_to_work_with = raw_input("Please enter a filename to analyze: ")
-
-#file = open(file_to_work_with,"r")
-
-#text = file.read()
-
-#file.close()
-
-#print text+"\n"
 text = re.sub(r'([^\s\w]|_)+', '', text)
 text = text.lower()
 total_letters = len(text)
@@ -25,7 +16,7 @@ for i, c in enumerate(text):
 			a[c] = a[c] + 1
 		else:
 			a[c] = 1
-			
+
 output = open(sys.argv[1]+".entropy","w")
 sum = 0.0
 for x, y in sorted(a.iteritems()):
@@ -39,4 +30,4 @@ output.close()
 sum = sum*-1
 
 print sum
-	
+
